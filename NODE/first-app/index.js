8e78a1639fa9e9d348e106cab455d4b62caf1a73
Allan <allan.bernier1@gmail.com>
2024-06-07 14:51:03 +0200

    http params

--- NODE/first-app/index.js
+++ NODE/first-app/index.js
@@ -3,9 +3,13 @@
 // $npm i express
 // package.json -> script "start" : "nodee index.js"
 // npm run start
-
+const bodyParser = require('body-parser')
 const express = require("express")
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 
 app.listen(3000, () => {
   console.log("App started on port 3000")
@@ -19,3 +23,21 @@ app.get("/", (req, res) => {
 app.get("/test", (req, res) => {
   res.send("Testing ...")
 })
+
+app.get("/product/:id", (req, res) => {
+  console.log(req.params.id)
+  res.send("Product : " + req.params.id)
+})
+
+app.get("/product", (req, res) => {
+  console.log(req.query.q)
+  res.send("finding product that start with :" + req.query.q)
+})
+
+app.post("/product", (req,res) => {
+  console.log(req.body)
+  res.send(req.body)
+})
+
+
+
