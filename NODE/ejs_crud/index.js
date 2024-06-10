f6412943390f010cf9b15539a6bc8b1c6fff2390
Allan <allan.bernier1@gmail.com>
2024-06-10 16:04:58 +0200

    Product EJS CRUD

--- NODE/ejs_crud/index.js
+++ NODE/ejs_crud/index.js
@@ -2,6 +2,11 @@
 const express = require("express")
 const app = express()
 require('dotenv').config()
+const productRouter = require("./router/productRouter")
+
+
+app.use(express.urlencoded({ extended: true }) )
+app.use(express.json())
 
 const PORT = process.env.SERVER_PORT || 3001
 
@@ -9,15 +14,13 @@ app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
 })
 
-// app.use(express.static( './public'));
-// app.get("/", (req, res) => {
-//   res.render("index")
-// })
 
 app.set('view engine', 'ejs')
 app.set('views', './views')
 
+app.use('/product', productRouter)
+
 
 app.get("/", (req, res) => {
-  res.render("index")
+  res.render("index", { title: "Home" })
 })
