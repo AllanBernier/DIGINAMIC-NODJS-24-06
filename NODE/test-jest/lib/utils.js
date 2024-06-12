2fc1e05cf9077a99fbe99ff4863ba799aa88523e
Allan <allan.bernier1@gmail.com>
2024-06-12 14:24:20 +0200

    Jest

--- NODE/test-jest/lib/utils.js
+++ NODE/test-jest/lib/utils.js
@@ -10,11 +10,36 @@ const substract = (a, b) => {
 const multiply = (a,b) => {
   return a * b
 }
-
+ 
 const toUpperCase = (str) => {
   return str.toUpperCase()
 }
 
 
-module.exports = {sum, substract, multiply, toUpperCase}
+const divide = (a,b) => {
+  if (b === 0)
+    throw new Error("Division by zero")
+
+  return a/b
+}
+
+const modulo = (a,b) => {
+  return a%b
+}
+
+const reverseString = (str) => {
+  return str.split('').reverse().join('')
+}
+
+const isPalindrome = (str) => {
+  return str === reverseString(str)
+}
+
+
+const isEven = (num) => {
+  return num % 2 === 0
+}
+
+
+module.exports = {sum, substract, multiply, toUpperCase, divide, isEven, isPalindrome, modulo, reverseString}
 
